04fe278fa3ab75390ed927a0e62ec8db278fdd2f
Sulabasana <piotr.frydryszak@gmail.com>
2016-12-09 12:24:42 +0100

    HTML is ready, css in progress, and js doesn't exist

---
+++ app.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function(){
+	// canvas = document.createElement('canvas');
+	// canvas.width= canvas.height = 200
+	// ctx = canvas.getContext("2d");
+
+	// document.body.appendChild(canvas);
+	// init();
+	// tick();
+
+	// function init(){
+
+	// }
+	// function tick(){
+	// 	window.requestAnimationFrame(tick);
+	// 	update();
+	// 	render();
+	// }
+	// function update() {};
+	// function render() {};
+
+	// function Title(x,y){
+	// 	var x = x, y = y;
+	// 	var tile = Tile.BLANK;
+
+	// 	if(tile == null){
+	// 		var _c = document.createElement('canvas');
+	// 		_c.width = _c.height = 100;
+	// 		_ctx = _c.getContext('2d');
+	// 		_ctx.fillStyle = "blue";
+	// 		//Blank
+	// 		_ctx.fillRect(0,0,100,100);
+	// 		Title.BLANK = new Image();
+	// 		Title.BLANK.src = _c.toDataURL();
+	// 	}
+
+	// 	this.update = function(){
+
+	// 	}
+	// 	this.draw = function(ctx){
+	// 		ctx.drawImage(tile,x,y)
+	// 	}
+	// }
+});
---
+++ readme.md
@@ -0,0 +1,1 @@
+TicTacToe - Well-known and popular game created in JS. I hope you are gonna enjoy it. Download as zip, unpack end play:)
