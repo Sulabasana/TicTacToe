611431c7dde3a5438a5628737be1cec05d1493d8
Sulabasana <piotr.frydryszak@gmail.com>
2016-12-11 16:46:09 +0100

    updated HTML, CSS animation with girandoles and js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 	var button = []
 	for(var i =0; i< 10;i++){
 		button[i]=document.getElementById('canvas'+ i);
-		console.log(button[i])
+		// console.log(button[i])
 	}
 	var ctx = [];
 	for(var i =1; i<10;i++){
@@ -22,8 +22,21 @@
 			bDisabled[x] = true;//niechcemy żeby ten sam przycisk był dostepny do kliknięcia cały czas
 			button[x].style.opacity = 0.7;//każdy przycisk po kliknięciu zmieni wygląd
 			content[x] = 'x';//to zdecyduje o zwycięstwie
-			
-			
+
+			button[x].style.webkitTransform = "rotateY(180deg)";
 		}
+		setTimeout(function(){ //dla opóźnienia
+			ctx[x].lineWidth = 3;//grubośc lini
+			ctx[x].beginPath(); //zacznij
+			ctx[x].moveTo(10,10); //punkt początkowy (x,y)
+			ctx[x].lineTo(90,90);//punkt końcowy
+			ctx[x].moveTo(90,10);
+			ctx[x].lineTo(10,90);
+			ctx[x].stroke(); //rób linie
+			ctx[x].closePath();//koniec drogi
+
+			computerTunr();
+		},300)
+		checkWinner();
 	}
 // });
