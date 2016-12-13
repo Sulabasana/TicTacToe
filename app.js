6dc33cbbb68d8bf290d0e96e5bb54cedd1973a42
Sulabasana <piotr.frydryszak@gmail.com>
2016-12-13 10:36:45 +0100

    Improved JS

--- app.js
+++ app.js
@@ -24,19 +24,61 @@
 			content[x] = 'x';//to zdecyduje o zwycięstwie
 
 			button[x].style.webkitTransform = "rotateY(180deg)";
-		}
-		setTimeout(function(){ //dla opóźnienia
-			ctx[x].lineWidth = 3;//grubośc lini
-			ctx[x].beginPath(); //zacznij
-			ctx[x].moveTo(10,10); //punkt początkowy (x,y)
-			ctx[x].lineTo(90,90);//punkt końcowy
-			ctx[x].moveTo(90,10);
-			ctx[x].lineTo(10,90);
-			ctx[x].stroke(); //rób linie
-			ctx[x].closePath();//koniec drogi
+			setTimeout(function(){ //dla opóźnienia
+				ctx[x].lineWidth = 3;//grubośc lini
+				ctx[x].beginPath(); //zacznij
+				ctx[x].moveTo(10,10); //punkt początkowy (x,y)
+				ctx[x].lineTo(90,90);//punkt końcowy
+				ctx[x].moveTo(90,10);
+				ctx[x].lineTo(10,90);
+				ctx[x].stroke(); //rób linie
+				ctx[x].closePath();//koniec drogi
 
-			computerTunr();
-		},300)
+				computerTurn();
+			},300)
 		checkWinner();
+		}
+		
 	}
+function computerTurn(){
+	var r = Math.random();
+
+	if(r< 0.1 && !bDisabled[1]) {  //jeżeli r jest mniejsze od 0.1 i bDisablde jest FALSE to uruchom fukcje
+		draw0Steps(1) //to wywołaj funkcję draw0Steps na 1 elemencie
+	}else if(r< 0.2 && !bDisabled[2]) draw0Steps(2);
+	else if(r< 0.3 && !bDisabled[3]) draw0Steps(3);
+	else if(r< 0.4 && !bDisabled[4]) draw0Steps(4);
+	else if(r< 0.5 && !bDisabled[5]) draw0Steps(5);
+	else if(r< 0.6 && !bDisabled[6]) draw0Steps(6);
+	else if(r< 0.7 && !bDisabled[7]) draw0Steps(7);
+	else if(r< 0.8 && !bDisabled[8]) draw0Steps(8);
+	else if(r< 0.1 && !bDisabled[9]) draw0Steps(9);
+	else computerTurn();
+}
+function draw0Steps(x){
+	bDisabled[x] = true; //jeżeli 0 zostanie narysowane na pozycji nr 5 to zablokuj przycisk
+	button[x].style.opacity = 0.7;
+	content[x]= '0'; //kto wygrał
+	button[x].style.webkitTransform = "rotateX(180deg)";
+
+	setTimeout(function(){
+
+	ctx[x].beginPath();
+	ctx[x].lineWidth = 3;
+	ctx[x].arc(50,50,34,0,Math.PI*2,false);
+	ctx[x].stroke();
+	ctx[x].closePath();
+	}, 300)
+}
+function checkWinner(){
+	if(!isResult){//isResult === false
+		if(content[1] == 'x' && content[2] == 'x' && content[3] == "x"){
+		showWinner("You win!");
+		}
+	}
+}
+function showWinner(x){
+	alert(x);
+	isResult = true;
+}
 // });
