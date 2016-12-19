b8018e58dc34ccc71a1207eba694def64c832627
Sulabasana <piotr.frydryszak@gmail.com>
2016-12-19 09:33:23 +0100

    Finish

--- app.js
+++ app.js
@@ -24,6 +24,9 @@
 			content[x] = 'x';//to zdecyduje o zwycięstwie
 
 			button[x].style.webkitTransform = "rotateY(180deg)";
+			button[x].style.MozTransform = "rotateY(180deg)";
+			button[x].style.OTransform = "rotateY(180deg)";
+			button[x].style.msTransform = "rotateY(180deg)";
 			setTimeout(function(){ //dla opóźnienia
 				ctx[x].lineWidth = 3;//grubośc lini
 				ctx[x].beginPath(); //zacznij
@@ -42,17 +45,47 @@
 	}
 function computerTurn(){
 	var r = Math.random();
+	if(content[1] == 'x' && content[2] == 'x' && !bDisabled[3]) draw0Steps(3);// 1 z 24 założeń
+	else if(content[1] == 'x' && content[3] == 'x' && !bDisabled[2]) draw0Steps(2);
+	else if(content[2] == 'x' && content[3] == 'x' && !bDisabled[1]) draw0Steps(1);
 
-	if(r< 0.1 && !bDisabled[1]) {  //jeżeli r jest mniejsze od 0.1 i bDisablde jest FALSE to uruchom fukcje
-		draw0Steps(1) //to wywołaj funkcję draw0Steps na 1 elemencie
-	}else if(r< 0.2 && !bDisabled[2]) draw0Steps(2);
+	else if(content[4] == 'x' && content[5] == 'x' && !bDisabled[6]) draw0Steps(6);
+	else if(content[5] == 'x' && content[6] == 'x' && !bDisabled[4]) draw0Steps(4);
+	else if(content[4] == 'x' && content[6] == 'x' && !bDisabled[5]) draw0Steps(5);
+
+	else if(content[7] == 'x' && content[8] == 'x' && !bDisabled[9]) draw0Steps(9);
+	else if(content[7] == 'x' && content[9] == 'x' && !bDisabled[8]) draw0Steps(8);
+	else if(content[8] == 'x' && content[9] == 'x' && !bDisabled[7]) draw0Steps(7);
+
+	else if(content[1] == 'x' && content[4] == 'x' && !bDisabled[7]) draw0Steps(7);
+	else if(content[4] == 'x' && content[7] == 'x' && !bDisabled[1]) draw0Steps(1);
+	else if(content[1] == 'x' && content[7] == 'x' && !bDisabled[4]) draw0Steps(4);
+
+	else if(content[2] == 'x' && content[5] == 'x' && !bDisabled[8]) draw0Steps(8);
+	else if(content[2] == 'x' && content[8] == 'x' && !bDisabled[5]) draw0Steps(5);
+	else if(content[5] == 'x' && content[8] == 'x' && !bDisabled[2]) draw0Steps(2);
+
+	else if(content[3] == 'x' && content[6] == 'x' && !bDisabled[9]) draw0Steps(9);
+	else if(content[3] == 'x' && content[9] == 'x' && !bDisabled[6]) draw0Steps(6);
+	else if(content[6] == 'x' && content[9] == 'x' && !bDisabled[3]) draw0Steps(3);
+
+	else if(content[1] == 'x' && content[5] == 'x' && !bDisabled[9]) draw0Steps(9);
+	else if(content[1] == 'x' && content[9] == 'x' && !bDisabled[5]) draw0Steps(5);
+	else if(content[5] == 'x' && content[9] == 'x' && !bDisabled[1]) draw0Steps(1);
+
+	else if(content[3] == 'x' && content[5] == 'x' && !bDisabled[7]) draw0Steps(7);
+	else if(content[3] == 'x' && content[7] == 'x' && !bDisabled[5]) draw0Steps(5);
+	else if(content[5] == 'x' && content[7] == 'x' && !bDisabled[3]) draw0Steps(3);
+	// if(r< 0.1 && !bDisabled[1]) {  //jeżeli r jest mniejsze od 0.1 i bDisablde jest FALSE to uruchom fukcje
+	// 	draw0Steps(1) //to wywołaj funkcję draw0Steps na 1 elemencie
+	else if(r< 0.2 && !bDisabled[2]) draw0Steps(2);
 	else if(r< 0.3 && !bDisabled[3]) draw0Steps(3);
 	else if(r< 0.4 && !bDisabled[4]) draw0Steps(4);
 	else if(r< 0.5 && !bDisabled[5]) draw0Steps(5);
 	else if(r< 0.6 && !bDisabled[6]) draw0Steps(6);
 	else if(r< 0.7 && !bDisabled[7]) draw0Steps(7);
 	else if(r< 0.8 && !bDisabled[8]) draw0Steps(8);
-	else if(r< 0.9 && !bDisabled[9]) draw0Steps(9);
+	else if(r< 1 && !bDisabled[9]) draw0Steps(9);
 	else computerTurn();
 }
 function draw0Steps(x){
@@ -60,6 +93,9 @@ function draw0Steps(x){
 	button[x].style.opacity = 0.7;
 	content[x]= '0'; //kto wygrał
 	button[x].style.webkitTransform = "rotateX(180deg)";
+	button[x].style.MozTransform = "rotateX(180deg)";
+	button[x].style.OTransform = "rotateX(180deg)";
+	button[x].style.msTransform = "rotateX(180deg)";
 
 	setTimeout(function(){
 
@@ -89,6 +125,20 @@ function checkWinner(){
 		else if(content[3] == '0' && content[6] == '0' && content[9] == "0") showWinner("You lose!");
 		else if(content[1] == '0' && content[5] == '0' && content[9] == "0") showWinner("You lose!");
 		else if(content[3] == '0' && content[5] == '0' && content[7] == "0") showWinner("You lose!");
+		
+		else if(
+			(content[1] == 'x'|| content[1]== '0') &&
+			(content[2] == 'x'|| content[2]== '0') &&
+			(content[3] == 'x'|| content[3]== '0') &&
+			(content[4] == 'x'|| content[4]== '0') &&
+			(content[5] == 'x'|| content[5]== '0') &&
+			(content[6] == 'x'|| content[6]== '0') &&
+			(content[7] == 'x'|| content[7]== '0') &&
+			(content[8] == 'x'|| content[8]== '0') &&
+			(content[9] == 'x'|| content[9]== '0')
+		)
+		showWinner("Draw! Play again!");
+		
 	}
 }
 function showWinner(x){
